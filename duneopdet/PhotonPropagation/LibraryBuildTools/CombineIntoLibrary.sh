2e8179499d27aae7f65e709048bc97ee28bedfd2
Jason Stock <jstock@dunegpvm05.fnal.gov>
2017-10-08 22:21:42 -0500

    Removing hacky debugging lines from CombineIntoLibrary.sh which were used in the last library generation. A real and more general debugging system for this script wouldn't be a bad idea in the future.

--- duneopdet/PhotonPropagation/LibraryBuildTools/CombineIntoLibrary.sh
+++ duneopdet/PhotonPropagation/LibraryBuildTools/CombineIntoLibrary.sh
@@ -9,9 +9,6 @@ list=temp${BatchID}_FileList.txt
 #Produce the list of files to combine
 ls -1 $where/root/*.root > $list
 
-echo "Waiting for keypress to continue."
-read -n 1 -s
-
 root -b -q "AssembleSingleFile.C+g(\"$list\", \"\",\"lib_$BatchID.root\")"
 
 cp lib_$BatchID.root $where
