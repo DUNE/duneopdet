c23cf1fff2f36459958d47f8ac517f4632c1d406
Jason Stock <jstock@dunegpvm05.fnal.gov>
2017-10-08 20:05:02 -0500

    Updating the recently added check_nEntries tool used to make sure than not EVERY voxel for every detector is filled (if they are filled, there is a problem and the files are unusably slow).

--- duneopdet/PhotonPropagation/LibraryBuildTools/check_nEntries.sh
+++ duneopdet/PhotonPropagation/LibraryBuildTools/check_nEntries.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 where=$1
+max=$2
 cr=0
 for file in ${where}/*.root;
   do
-    if [ $cr -lt 3 ]; then
+    if [ $cr -lt $max ]; then
     toOpen=$file
-    root -b -q "CountEntries.C(\"${toOpen}\")"
+    root -l -b -q "CountEntries.C+g(\"${toOpen}\")"
   fi
   cr=$((cr+1))
   done
