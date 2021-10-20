bbb15b68ec4dae271703bfac9c18bf75f0615596
Thomas Junk <trj@fnal.gov>
2021-10-19 19:37:08 -0500

    more getByLabel to getHandle changes

--- duneopdet/OpticalDetector/AverageWaveform_module.cc
+++ duneopdet/OpticalDetector/AverageWaveform_module.cc
@@ -138,8 +138,8 @@ namespace opdet {
     {
 
         // Get OpDetWaveforms from the event
-        art::Handle< std::vector< raw::OpDetWaveform > > waveformHandle;
-        evt.getByLabel(fInputModule, fInstanceName, waveformHandle);
+        art::InputTag itag1(fInputModule, fInstanceName);
+        auto waveformHandle = evt.getHandle< std::vector< raw::OpDetWaveform > >(itag1);
 
         // Access ART's TFileService, which will handle creating and writing
         // histograms for us
