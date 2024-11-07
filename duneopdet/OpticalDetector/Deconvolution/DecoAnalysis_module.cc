0ad78a92199b1b58c2d2289479779d3ca104ca58
Dom Brailsford <dominicbrailsford@gmail.com>
2024-11-07 03:19:57 -0600

    Reapply "Adding capability to deconvolve using channel-by-channel templates"
    This reverts commit 7eb23d6d9f8e7130846d450816385da8cff6ded6.

--- duneopdet/OpticalDetector/Deconvolution/DecoAnalysis_module.cc
+++ duneopdet/OpticalDetector/Deconvolution/DecoAnalysis_module.cc
@@ -3,12 +3,17 @@
 // This analyzer module for Deconvolution creates histograms
 // with information from OpDetWaveforms and OpWaveforms.
 // @authors     : Daniele Guffanti, Maritza Delgado, Sergio Manthey Corchado
-// @created     : 2022 
-//=========================================================================== 
+// @created     : 2022
+//
+// Modified: Oct 7, 2024, Viktor Pec
+//     Added raw waveforms to the output. Redid directory structure
+//     inside the root file. Set the maximum number of histograms to
+//     save to 400.
+//     ===========================================================================
 
 #ifndef DecoAnalysis_h
 #define DecoAnalysis_h
- 
+
 // LArSoft includes
 #include "larcore/Geometry/Geometry.h"
 #include "lardataobj/RawData/OpDetWaveform.h"
@@ -24,11 +29,12 @@
 #include "art/Framework/Principal/Handle.h"
 #include "canvas/Persistency/Common/Ptr.h"
 #include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/FindOne.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art_root_io/TFileService.h"
 #include "art_root_io/TFileDirectory.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
-  
+
 // ROOT includes
 #include "TH1.h"
 #include "THStack.h"
@@ -36,7 +42,7 @@
 #include "TLorentzVector.h"
 #include "TVector3.h"
 #include "TTree.h"
-  
+
 // C++ Includes
 #include <map>
 #include <vector>
@@ -48,102 +54,136 @@
 
 namespace opdet {
   class DecoAnalysis : public art::EDAnalyzer{
-    public:
-      // Standard constructor and destructor for an ART module.
-      DecoAnalysis(const fhicl::ParameterSet&);
-      virtual ~DecoAnalysis();
-      // The analyzer routine, called once per event. 
-      void analyze (const art::Event&); 
+  public:
+    // Standard constructor and destructor for an ART module.
+    DecoAnalysis(const fhicl::ParameterSet&);
+    virtual ~DecoAnalysis();
+    // The analyzer routine, called once per event.
+    void analyze (const art::Event&);
 
-    private:
-      // The parameters we'll read from the .fcl file.
-      std::string fInputModuleDeco;          // Input tag for OpDet collection
-      std::string fInputModuleDigi;          // Input tag for OpDet collection
-      std::string fInstanceName;             // Input tag for OpDet collection
-      double fSampleFreq;                    // in MHz
+  private:
+    // The parameters we'll read from the .fcl file.
+    std::string fInputModuleDeco;          // Input tag for OpDet collection
+    std::string fInputModuleDigi;          // Input tag for OpDet collection
+    std::string fInstanceNameDeco;             // Input tag for OpDet collection
+    std::string fInstanceNameDigi;             // Input tag for OpDet collection
+    double fSampleFreq;                    // in MHz
     //float fTimeBegin;                      // in us
     // float fTimeEnd;                        // in us
-  };  
-} 
 
-#endif 
+    static const size_t MAX_WFMS = 400;
+  };
+}
+
+#endif
 namespace opdet {
   DEFINE_ART_MODULE(DecoAnalysis)
 }
-  
+
 namespace opdet {
   //-----------------------------------------------------------------------
   // Constructor
   DecoAnalysis::DecoAnalysis(fhicl::ParameterSet const& pset)
-  : EDAnalyzer(pset){
+    : EDAnalyzer(pset){
     // Read the fcl-file
     fInputModuleDeco  =   pset.get< std::string >("InputModuleDeco");
     fInputModuleDigi  =   pset.get< std::string >("InputModuleDigi");
-    fInstanceName     =   pset.get< std::string >("InstanceName");
-    
+    fInstanceNameDeco =   pset.get< std::string >("InstanceNameDeco");
+    fInstanceNameDigi =   pset.get< std::string >("InstanceNameDigi");
+
     // Obtain parameters from DetectorClocksService
-    auto const clockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataForJob();     
+    auto const clockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataForJob();
     fSampleFreq = clockData.OpticalClock().Frequency();
 
     art::ServiceHandle< art::TFileService > tfs;
   }
-  
+
   //-----------------------------------------------------------------------
   // Destructor
-  DecoAnalysis::~DecoAnalysis() {  
+  DecoAnalysis::~DecoAnalysis() {
   }
-    
+
   //-----------------------------------------------------------------------
-  void DecoAnalysis::analyze(const art::Event& evt){ 
+  void DecoAnalysis::analyze(const art::Event& evt){
+    auto mfi = mf::LogInfo("DecoAnalysis::analyze()");
+
+
     // Map to store how many waveforms are on one optical channel
     std::map< int, int > mapChannelWF;
-  
+
     // Get deconvolved "OpWaveforms" from the event
     art::Handle< std::vector< recob::OpWaveform >> deconv;
-    evt.getByLabel(fInputModuleDeco, fInstanceName, deconv);
+    evt.getByLabel(fInputModuleDeco, fInstanceNameDeco, deconv);
     std::vector< recob::OpWaveform > OpWaveform;
     for (auto const& wf : *deconv) {OpWaveform.push_back(wf);}
-    
+
     // Get Waveforms "OpDetWaveforms" from the event
-    art::Handle< std::vector< raw::OpDetWaveform >> digi;
-    evt.getByLabel(fInputModuleDigi, fInstanceName, digi);
-    std::vector< raw::OpDetWaveform > OpDetWaveform;
-    for (auto const& wf : *digi) {OpDetWaveform.push_back(wf);}
+    //art::Handle< std::vector< raw::OpDetWaveform >> digi;
+    art::FindOne<raw::OpDetWaveform> fopDigi(deconv, evt, fInputModuleDeco);
 
     // Access ART's TFileService, which will handle creating and writing histograms for us
     art::ServiceHandle< art::TFileService > tfs;
-    
-    double firstWaveformTime = -9999;
-    for (auto const& waveform : *digi)
-    {
-      if (firstWaveformTime < 0) firstWaveformTime = waveform.TimeStamp();
-    }
+    auto evt_dir = tfs->mkdir(TString::Format("run_%d_evt_%d", evt.id().run(), evt.id().event()).Data());
 
-    for (int i = 0; i < int(OpWaveform.size()); i++){
-      raw::OpDetWaveform waveform = OpDetWaveform.at(i);
-      recob::OpWaveform decowaveform = OpWaveform.at(i);
+    double firstWaveformTime = -9999;
+    for (long unsigned int i = 0; i < fopDigi.size(); ++i)
+      {
+	auto const &waveform = fopDigi.at(i).ref();
+	if (firstWaveformTime == -9999 || firstWaveformTime > waveform.TimeStamp())
+	  firstWaveformTime = waveform.TimeStamp();
+      }
+
+    std::map<int,std::pair<art::TFileDirectory, art::TFileDirectory> > chan_dir_map;
+
+
+    mfi<<"\n"
+       <<"Number of deconvolved waveforms in this event: "<<OpWaveform.size()
+       <<"\n";
+
+    size_t max_wfms = MAX_WFMS;
+    if (MAX_WFMS > OpWaveform.size())
+      max_wfms = OpWaveform.size();
+
+    mfi<<"Number of deconvolved waveforms to be saved in this event: "<<max_wfms<<"\n";
+
+    for (size_t i = 0; i < max_wfms; i++){
+      raw::OpDetWaveform const& waveform = fopDigi.at(i).ref();
+      recob::OpWaveform& decowaveform = OpWaveform.at(i);
       int channel = decowaveform.Channel();
 
       // Implement different end time for waveforms of variable length
       double startTime = waveform.TimeStamp() - firstWaveformTime;
       double endTime = double(waveform.size())/fSampleFreq + startTime;
 
-      // Make a name for the histogram
-      std::stringstream histName;
-      histName << "event_"      << evt.id().event() 
-              << "_opchannel_" << channel
-              << "_decowaveform_"  << mapChannelWF[channel];
-              
+
+      // create channel subdir
+      if (mapChannelWF[channel] == 0) {
+	chan_dir_map.emplace( channel,
+			      std::pair(evt_dir.mkdir(TString::Format("ch%d/raw", channel).Data()),
+					evt_dir.mkdir(TString::Format("ch%d/deconv", channel).Data())));
+      }
+
+      auto &chan_dir = chan_dir_map.at(channel);
+
       // Increase counter for number of waveforms on this optical channel
-      mapChannelWF[channel]++;
+      auto nth_waveform = mapChannelWF[channel]++;
+
 
       // Create a new histogram
-      TH1D *decowaveformHist = tfs->make< TH1D >(histName.str().c_str(),  TString::Format(";t - %f (#mus);",firstWaveformTime), decowaveform.NSignal(), startTime, endTime);
+      TH1D *decowaveformHist = chan_dir.second.make< TH1D >(TString::Format("waveform_%d", nth_waveform),  TString::Format(";t - %f (#mus);",firstWaveformTime), decowaveform.NSignal(), startTime, endTime);
       // Copy values from the waveform into the histogram
       for(size_t tick = 0; tick < decowaveform.NSignal(); tick++){
-        // Fill histogram with waveform                        
-        decowaveformHist->SetBinContent(tick, decowaveform.Signal()[tick]);
-      }                
-    }                                      
-  } 
+        // Fill histogram with waveform
+        decowaveformHist->SetBinContent(tick+1, decowaveform.Signal()[tick]);
+      }
+
+      // Create a new histogram with raw waveform
+      TH1D *rawwaveformHist = chan_dir.first.make< TH1D >(TString::Format("waveform_%d", nth_waveform),  TString::Format(";t - %f (#mus);",firstWaveformTime), waveform.Waveform().size(), startTime, endTime);
+      // Copy values from the waveform into the histogram
+      for(size_t tick = 0; tick < waveform.Waveform().size(); tick++){
+        // Fill histogram with waveform
+        rawwaveformHist->SetBinContent(tick+1, waveform.Waveform()[tick]);
+      }
+    }
+  }
 } // namespace opdet
