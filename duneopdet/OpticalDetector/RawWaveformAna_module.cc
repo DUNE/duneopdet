bba0e5c0e2c358be0204ba3de1d65816caf4186f
Viktor Pec <viktor.pec@fzu.cz>
2025-09-19 11:49:13 +0200

    Adding analyzer module to dump raw waveforms from into a simple ROOT TTree. This is similar to what the decoder (duneprototypes/Protodune/hd/RawDecoding/DAPHNEReaderPDHD_module.cc) does, but there not all the branches are populated.

---
+++ duneopdet/OpticalDetector/RawWaveformAna_module.cc
@@ -0,0 +1,156 @@
+//===========================================================================
+// RawWaveformAna_module.cc
+// This analyzer module for Deconvolution creates histograms
+// with information from OpDetWaveforms and OpWaveforms.
+// @authors     : Daniele Guffanti, Maritza Delgado, Sergio Manthey Corchado
+// @created     : 2022
+//
+// Modified: Oct 7, 2024, Viktor Pec
+//     Added raw waveforms to the output. Redid directory structure
+//     inside the root file. Set the maximum number of histograms to
+//     save to 400.
+//     ===========================================================================
+
+#ifndef RawWaveformAna_h
+#define RawWaveformAna_h
+
+// LArSoft includes
+#include "larcore/Geometry/Geometry.h"
+#include "lardataobj/RawData/OpDetWaveform.h"
+#include "lardataobj/RecoBase/OpHit.h"
+#include "lardataobj/RecoBase/OpWaveform.h"
+#include "lardata/DetectorInfoServices/DetectorClocksService.h"
+
+// Framework includes
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Principal/Handle.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/FindOne.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art_root_io/TFileService.h"
+#include "art_root_io/TFileDirectory.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// ROOT includes
+#include "TH1.h"
+#include "THStack.h"
+#include "TF1.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TTree.h"
+
+// C++ Includes
+#include <map>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include "math.h"
+#include <climits>
+
+namespace opdet {
+    class RawWaveformAna : public art::EDAnalyzer{
+    public:
+        // Standard constructor and destructor for an ART module.
+        RawWaveformAna(const fhicl::ParameterSet&);
+        virtual ~RawWaveformAna();
+        // The analyzer routine, called once per event.
+        void analyze (const art::Event&);
+
+    private:
+        // The parameters we'll read from the .fcl file.
+        std::string fInputModuleLabel;          // Input tag for OpDetWaveform collection
+        std::string fTriggerModuleLabel;          // Input tag for raw::Trigger
+
+        TTree* fWaveformTree;
+
+        int Run;
+        int SubRun;
+        int Event;
+        uint64_t TimeStamp;
+        int OpChannel;
+        unsigned int nSamples;
+        double SampleSize;
+        std::vector<short> adc_value;
+        unsigned int nOpDet;
+        int TriggerType;
+
+    };
+}
+
+#endif
+namespace opdet {
+    DEFINE_ART_MODULE(RawWaveformAna)
+}
+
+namespace opdet {
+    //-----------------------------------------------------------------------
+    // Constructor
+    RawWaveformAna::RawWaveformAna(fhicl::ParameterSet const& pset)
+        : EDAnalyzer(pset){
+        // Read the fcl-file
+        fInputModuleLabel  =   pset.get< std::string >("InputModule");
+        //fTriggerModuleLabel  =   pset.get< std::string >("TriggerModule");
+
+        art::ServiceHandle< art::TFileService > tfs;
+
+
+        fWaveformTree = tfs->make<TTree>("WaveformTree","Waveforms Tree");
+        fWaveformTree->Branch("Run"       , &Run       , "Run/I"       );
+        fWaveformTree->Branch("SubRun"    , &SubRun    , "SubRun/I"    );
+        fWaveformTree->Branch("Event"     , &Event     , "Event/I"     );
+        fWaveformTree->Branch("Trigger"     , &TriggerType     , "Trigger/I"     );
+        fWaveformTree->Branch("TimeStamp" , &TimeStamp     , "TimeStamp/l"     );
+        fWaveformTree->Branch("NSamples"     , &nSamples     , "NSamples/I"     );
+        fWaveformTree->Branch("OpChannel"     , &OpChannel     , "OpChannel/I"     );
+        fWaveformTree->Branch("adc", &adc_value);
+
+
+    }
+
+    //-----------------------------------------------------------------------
+    // Destructor
+    RawWaveformAna::~RawWaveformAna() {
+    }
+
+    //-----------------------------------------------------------------------
+    void RawWaveformAna::analyze(const art::Event& evt){
+        auto mfi = mf::LogInfo("RawWaveformAna::analyze()");
+
+
+        // Map to store how many waveforms are on one optical channel
+        std::map< int, int > mapChannelWF;
+
+        // Get deconvolved "OpWaveforms" from the event
+        art::Handle< std::vector< raw::OpDetWaveform >> wfmHndl;
+        evt.getByLabel(fInputModuleLabel, wfmHndl);
+
+        // auto trigHndl = evt.getHandle< std::vector<raw::Trigger>>(fTriggerModuleLabel);
+        // auto &trig = trigHndl[0];
+
+        Run = evt.id().run();
+        SubRun = evt.id().subRun();
+        Event = evt.id().event();
+        TriggerType = 0;
+
+        for (auto &wfm: *wfmHndl) {
+            OpChannel = wfm.ChannelNumber();
+            nSamples  = wfm.Waveform().size();
+            TimeStamp = (uint64_t)wfm.TimeStamp();
+            adc_value.resize(nSamples);
+
+            for (unsigned int ii = 0; ii< nSamples ; ii++) {
+                adc_value[ii] = wfm.Waveform()[ii];
+            }
+
+
+            fWaveformTree->Fill();
+        }
+
+
+    }
+} // namespace opdet
